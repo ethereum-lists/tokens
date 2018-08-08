f29af7cf5b2776f49b3537638b4843bd2b40805b
ligi <ligi@ligi.de>
2018-08-08 21:45:53 +0200

    Add new optional field "type" used by MEW

--- src/main/kotlin/org/ethereum/lists/tokens/Constant.kt
+++ src/main/kotlin/org/ethereum/lists/tokens/Constant.kt
@@ -5,7 +5,7 @@ import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import java.io.File
 
 val mandatoryFields = listOf("name", "symbol", "address", "decimals")
-val optionalFields = listOf("comment", "logo", "support", "community", "website", "github", "img-16x16", "img-128x128", "social", "ens_address", "deprecation")
+val optionalFields = listOf("comment", "logo", "support", "community", "website", "github", "img-16x16", "img-128x128", "social", "ens_address", "deprecation", "type")
 
 val outDir = File("build/output")
 val allNetworksTokenDir = File("tokens")
