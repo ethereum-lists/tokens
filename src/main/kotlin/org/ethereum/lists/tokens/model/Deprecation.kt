6463e62f889aece132b97ba230032a0c83515fb3
ligi <ligi@ligi.de>
2019-03-08 19:36:54 +0100

    Make new_address in deprecation optional - context #201

--- src/main/kotlin/org/ethereum/lists/tokens/model/Deprecation.kt
+++ src/main/kotlin/org/ethereum/lists/tokens/model/Deprecation.kt
@@ -1,7 +1,7 @@
 package org.ethereum.lists.tokens.model
 
 data class Deprecation(
-        val new_address: String,
+        val new_address: String?,
         val announcement_url: String? = null,
         val time: String? = null, // ISO8601
         val migration_type: String? = null // currently only "auto" is allowed
