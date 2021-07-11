94725efe3b01dbc9ae7f439b2d6e54144a5d2ae4
kohansal6396 <mahmoud.kohansal@gmail.com>
2021-07-11 17:23:38 +0430

    Get Pre-Existing Erc20 Tokens

--- ProkeyCoinsInfoGrabber/Program.cs
+++ ProkeyCoinsInfoGrabber/Program.cs
@@ -1,12 +1,35 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace ProkeyCoinsInfoGrabber
 {
     class Program
     {
+        public static string ERC20TOKENS_DIRECTORY_PATH = "tokens\\eth";
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Get eth directory file names(ERC20 Token addresses) as an array
+            List<string> erc20TokenfileName_List = GetPreExistingErc20Tokens();
+            foreach (var item in erc20TokenfileName_List)
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+        private static List<string> GetPreExistingErc20Tokens()
+        {
+            List<string> erc20TokenfileName_List = new List<string>();
+            string erc20TokensDirctoryAbsolutePath = System.IO.Path.Combine(System.IO.Directory.GetParent(System.IO.Directory.GetCurrentDirectory()).Parent.Parent.Parent.FullName, ERC20TOKENS_DIRECTORY_PATH);
+            string[] erc20TokenfileNames = System.IO.Directory.GetFiles(erc20TokensDirctoryAbsolutePath);
+            foreach (string tokenFileName in erc20TokenfileNames)
+            {
+                string[] fileNameParts = tokenFileName.Split(System.IO.Path.DirectorySeparatorChar);
+                string fileNameWithExt = fileNameParts[^1];
+                char[] trimJsonChars = new char[] { '.', 'j', 's', 'o', 'n' };
+                string fileNameWithoutExt = fileNameWithExt.TrimEnd(trimJsonChars);
+                erc20TokenfileName_List.Add(fileNameWithoutExt);
+            }
+            return erc20TokenfileName_List;
         }
     }
 }
