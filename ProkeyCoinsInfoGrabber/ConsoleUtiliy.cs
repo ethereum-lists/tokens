ee85bcdfc987c20044887444476a634ad9092793
kohansal6396 <mahmoud.kohansal@gmail.com>
2021-07-11 18:13:14 +0430

    Console Utility LogInfo

---
+++ ProkeyCoinsInfoGrabber/ConsoleUtiliy.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProkeyCoinsInfoGrabber
+{
+    public static class ConsoleUtiliy
+    {
+        public static void LogInfo(string message)
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine(message);
+            Console.ResetColor();
+        }
+    }
+}
--- ProkeyCoinsInfoGrabber/Program.cs
+++ ProkeyCoinsInfoGrabber/Program.cs
@@ -9,15 +9,13 @@ namespace ProkeyCoinsInfoGrabber
         static void Main(string[] args)
         {
             //Get eth directory file names(ERC20 Token addresses) as an array
-            List<string> erc20TokenfileName_List = GetPreExistingErc20Tokens();
-            foreach (var item in erc20TokenfileName_List)
-            {
-                Console.WriteLine(item);
-            }
+            List<string> erc20TokenfileName_List = GetPreExistingErc20Tokens();            
+            
         }
 
         private static List<string> GetPreExistingErc20Tokens()
         {
+            ConsoleUtiliy.LogInfo("Getting pre-existing Erc20 tokens...");
             List<string> erc20TokenfileName_List = new List<string>();
             string erc20TokensDirctoryAbsolutePath = System.IO.Path.Combine(System.IO.Directory.GetParent(System.IO.Directory.GetCurrentDirectory()).Parent.Parent.Parent.FullName, ERC20TOKENS_DIRECTORY_PATH);
             string[] erc20TokenfileNames = System.IO.Directory.GetFiles(erc20TokensDirctoryAbsolutePath);
