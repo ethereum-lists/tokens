b943da7ca3f9f4c832ddd8b7ec0c3cca38c52b94
kohansal6396 <mahmoud.kohansal@gmail.com>
2021-07-14 10:35:10 +0430

    Modify Directory Path
    Use ../ instead of Directory.GetParent/Directory.Parent

--- ProkeyCoinsInfoGrabber/Program.cs
+++ ProkeyCoinsInfoGrabber/Program.cs
@@ -13,10 +13,12 @@ namespace ProkeyCoinsInfoGrabber
 {
     class Program
     {
-        public static string ERC20TOKENS_DIRECTORY_PATH = System.IO.Path.Combine(System.IO.Directory.GetParent(System.IO.Directory.GetCurrentDirectory()).Parent.Parent.Parent.FullName, "tokens\\eth");
+        //System.IO.Path.Combine(System.IO.Directory.GetParent(System.IO.Directory.GetCurrentDirectory()).Parent.Parent.Parent.FullName, "tokens\\eth");
+        public static string ERC20TOKENS_DIRECTORY_PATH = "../../../../tokens/eth";
         public static int HOW_MANY_POPULAR_TOKEN_PAGES = 1;
         public static string COINGECKO_LISTCOINS_API_URL = "https://api.coingecko.com/api/v3/coins/list?include_platform=true";
-        public static string COINS_HAVE_LANDINGPAGE_PATH = System.IO.Path.Combine(System.IO.Directory.GetParent(System.IO.Directory.GetCurrentDirectory()).Parent.Parent.FullName,"data\\landingPageList.txt");
+        //System.IO.Path.Combine(System.IO.Directory.GetParent(System.IO.Directory.GetCurrentDirectory()).Parent.Parent.FullName,"data\\landingPageList.txt");
+        public static string COINS_HAVE_LANDINGPAGE_PATH = "../../../../data/landingPageList.txt";
         public static string ETHPLORER_APIKEY = "EK-nFPsq-fhXwSwW-CofSq";
 
         static void Main(string[] args)
@@ -83,15 +85,24 @@ namespace ProkeyCoinsInfoGrabber
             List<string> erc20TokenfileName_List = new List<string>();
             try
             {
-                string[] erc20TokenfileNames = System.IO.Directory.GetFiles(erc20TokensDirctoryAbsolutePath);
-                foreach (string tokenFileName in erc20TokenfileNames)
+                if (System.IO.Directory.Exists(erc20TokensDirctoryAbsolutePath))
                 {
-                    string[] fileNameParts = tokenFileName.Split(System.IO.Path.DirectorySeparatorChar);
-                    string fileNameWithExt = fileNameParts[^1];
-                    char[] trimJsonChars = new char[] { '.', 'j', 's', 'o', 'n' };
-                    string fileNameWithoutExt = fileNameWithExt.TrimEnd(trimJsonChars);
-                    erc20TokenfileName_List.Add(fileNameWithoutExt);
+                    string[] erc20TokenfileNames = System.IO.Directory.GetFiles(erc20TokensDirctoryAbsolutePath);
+                    foreach (string tokenFileName in erc20TokenfileNames)
+                    {
+                        string[] fileNameParts = tokenFileName.Split(System.IO.Path.DirectorySeparatorChar);
+                        string fileNameWithExt = fileNameParts[^1];
+                        char[] trimJsonChars = new char[] { '.', 'j', 's', 'o', 'n' };
+                        string fileNameWithoutExt = fileNameWithExt.TrimEnd(trimJsonChars);
+                        erc20TokenfileName_List.Add(fileNameWithoutExt);
+                    }
                 }
+                else
+                {
+                    ConsoleUtiliy.LogError($"Directory {erc20TokensDirctoryAbsolutePath} not found!");
+                    return null;
+                }
+                
             }
             catch (System.IO.DirectoryNotFoundException)
             {
