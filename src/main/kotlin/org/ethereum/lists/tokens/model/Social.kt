ed1a5f7d1ddcc6b75265f0b0265e38440c1b4e4a
ligi <ligi@ligi.de>
2021-12-10 23:39:19 +0100

    Improve order

--- src/main/kotlin/org/ethereum/lists/tokens/model/Social.kt
+++ src/main/kotlin/org/ethereum/lists/tokens/model/Social.kt
@@ -3,19 +3,20 @@ package org.ethereum.lists.tokens.model
 data class Social(
         val blog: String? = null,
         val chat: String? = null,
-        val facebook: String? = null,
         val forum: String? = null,
         val discord: String? = null,
         val github: String? = null,
         val gitter: String? = null,
-        val instagram: String? = null,
-        val linkedin: String? = null,
         val reddit: String? = null,
-        val slack: String? = null,
-        val telegram: String? = null,
         val twitter: String? = null,
         val medium: String? = null,
         val bitcointalk: String? = null,
+        val slack: String? = null,
+        val telegram: String? = null,
         val googleplus: String? = null,
         val vimeo: String? = null,
-        val youtube: String? = null)
+        val youtube: String? = null,
+        val instagram: String? = null,
+        val linkedin: String? = null,
+        val facebook: String? = null
+)
